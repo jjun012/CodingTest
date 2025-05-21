f909c480ef5f466d28bf983364f08427ab8656d3
jjun012 <163534242+jjun012@users.noreply.github.com>
2025-05-21 21:27:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 176 ms, Memory: 110728 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,13 +1,14 @@
-a= int(input())
+n=int(input())
 cnt=0
 result=666
 
 while True:
-    if '666' in str(result):
+    if'666'in str(result):
         cnt+=1
-    if cnt ==a:
+    if cnt==n:
         break
 
-    result +=1
-
+    result+=1
 print(result)
+
+
