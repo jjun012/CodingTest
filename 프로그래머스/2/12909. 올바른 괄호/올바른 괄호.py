6564264a9092450eaafb24c6d31da6b93e14544f
jjun012 <163534242+jjun012@users.noreply.github.com>
2026-05-22 11:34:41 +0900

    [level 2] Title: 올바른 괄호, Time: 6.68 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    stack=[]
+    for i in s:
+        if i=="(":
+            stack.append("(")
+        else:
+            if stack==[]:
+                return False
+            else:
+                stack.pop()
+    if stack!=[]:
+        return False
+    else:
+        return True
+            
+        
+            
+    
